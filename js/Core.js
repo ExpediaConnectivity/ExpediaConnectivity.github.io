b17285f35407b925ae5eee42c90128d4b71f4f65
TeamCity <teamcity@expedia.com>
2016-02-09 18:55:47 -0800

    git publish

--- js/Core.js
+++ js/Core.js
@@ -59,17 +59,17 @@ var Core = Core || {
             if ($("html").hasClass("translated-ltr") || $("html").hasClass("translated-rtl")) {
                 lang = google.translate.TranslateElement().f;
                 if (typeof lang != "undefined" && lang != this.previouslang) {
-                    ga('send', 'event', 'Google Translate', 'translate', 'developer.expediapartnercentral.com/translate', lang, {
+                    result = ga('send', 'event', 'Google Translate', 'translate', 'developer.expediapartnercentral.com/translate', lang, {
                         nonInteraction: true
                     });
-                    console.log("Translated to " + lang + " (from " + this.previouslang + ")");
+                    console.log("Translated to " + lang + " (from " + this.previouslang + ") result: " + result);
                     this.previouslang = lang;
                 }
             } else if (typeof this.previouslang != "undefined") {
-                ga('send', 'event', 'Google Translate', 'translate', 'developer.expediapartnercentral.com/translate', "en", {
+                result = ga('send', 'event', 'Google Translate', 'translate', 'developer.expediapartnercentral.com/translate', "en", {
                     nonInteraction: true
                 });
-                console.log("Translate returned to en from " + this.previouslang);
+                console.log("Translate returned to en (from " + this.previouslang + ") result: " + result);
                 delete this.previouslang;
             }
         } catch(e) {
