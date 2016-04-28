2dfa7a08b82d46abc4127b4e03d1a30bc6658996
TeamCity <teamcity@expedia.com>
2016-04-28 01:49:12 +0000

    git publish

---
+++ js/featureSwitch.js
@@ -0,0 +1,38 @@
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+
+var getParams = getSearchParameters();
+
+function setSurveyFeature() {
+    localStorage.setItem("survey", "true");
+    $("#nav-main #menu-toggle").show();
+    $("#nav-main #menu-wrap").show();
+    $("#nav-main a#logo").attr("href", "/home");
+    console.log("Survey feature set.");
+}
+
+function unsetSurveyFeature() {
+    localStorage.removeItem('survey');
+    console.log("Survey feature unset.");
+}
+
+
+$(document).ready(function() {
+    if (getParams.survey == 'false') {
+        unsetSurveyFeature();
+    } else if (getParams.hasOwnProperty('survey') || localStorage.getItem("survey") != null) {
+        setSurveyFeature();
+    }
+});
