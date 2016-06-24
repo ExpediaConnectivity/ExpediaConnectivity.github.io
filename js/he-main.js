bf8d01f59bea00d2e46fb7fa0da4954d986ec3d9
TeamCity <teamcity@expedia.com>
2016-06-24 05:46:22 +0000

    git publish

--- js/he-main.js
+++ js/he-main.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
     
-    var menu = 0;
     $('#menu-toggle').on('click', function(){
         $('#menu-wrap').slideToggle(500);
         $(this).hide().toggleClass('open').fadeIn();
     })
-    $(window).on('resize', function(){
-        var window_width = $(window).width();
-        if ( window_width >= 865 ) {
-            $('#menu-wrap').show();
-            $('#menu-toggle').removeClass('open');
-        } else {
-            $('#menu-wrap').slideUp(0);
-        }
-    });
-    
+   
 });
