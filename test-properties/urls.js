8bcff05f4c68a5aef5a9832f912c4efa9f409f20
TeamCity <teamcity@expedia.com>
2016-09-26 02:15:15 +0000

    git publish

--- test-properties/urls.js
+++ test-properties/urls.js
@@ -1,13 +1,26 @@
-var hotelAssignmentServiceBaseUrl = "https://hotel-assignment-service.test.expedia.com";
+function hotelAssignmentServiceBaseUrl() {
+    return environment.isProd() ? "https://hotel-assignment-service.prod-p.expedia.com" : "https://hotel-assignment-service.test.expedia.com";
+}
 
 var hotelAssignmentServiceUrls = {
-    login: hotelAssignmentServiceBaseUrl + "/v1/properties/login",
-    schedule: hotelAssignmentServiceBaseUrl + "/v1/schedule",
-    factors: hotelAssignmentServiceBaseUrl + "/v1/hotels/factors",
-    assign: hotelAssignmentServiceBaseUrl + "/v1/schedule/assign",
+    login: function() {
+        return hotelAssignmentServiceBaseUrl() + "/v1/properties/login",
+    },
+
+    schedule: function() {
+        return hotelAssignmentServiceBaseUrl() + "/v1/schedule",
+    },
+
+    factors: function() {
+        return hotelAssignmentServiceBaseUrl() + "/v1/hotels/factors",
+    }
+
+    assign: function() {
+        return hotelAssignmentServiceBaseUrl() + "/v1/schedule/assign",
+    }
 
     scheduleHotel: function(hotelId) {
-        return hotelAssignmentServiceUrls.schedule + "/hotelId/" + hotelId;
+        return hotelAssignmentServiceUrls.schedule() + "/hotelId/" + hotelId;
     },
 
     scheduleExtend: function(hotelId) {
